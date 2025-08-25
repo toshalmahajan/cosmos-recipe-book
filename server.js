05af8472b3ba8ba9c3017eca4af3cd4575732406
EricKart <aryktripathi@gmail.com>
2025-08-25 10:33:56 +0530

    Fix: use dynamic port for Azure

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.delete('/api/recipes/:id', async (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
